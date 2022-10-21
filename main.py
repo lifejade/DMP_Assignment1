5d88b1980601a1cfef4743e2bd5fba7f49663fe2
Win10 <rft753@naver.com>
2022-10-21 17:47:28 +0900

    123

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import cv2
+import ctypes
 
 screensize = 680
 
@@ -29,17 +30,17 @@ def on_mouse(event, x, y, flags, param):
                     arrx * int(screensize / n) + int(screensize / n / 2 - queenSize[1] / 2)]
         board[startPos[0]:startPos[0] + queenSize[0],
         startPos[1]:startPos[1] + queenSize[1]] += queen
-        cv2.imshow('image', board)
+        cv2.imshow('board', board)
 
         if not checknQueen(arrx, arry):
-            print("you lose!!")
+            ctypes.windll.user32.MessageBoxW(0, "you lose!!", "game over")
             sys.exit(0)
 
         arr[arrx] = arry
         for i in range(0, n):
             if arr[i] == -1:
                 return
-        print("you win!")
+        ctypes.windll.user32.MessageBoxW(0, "YOU WIN!!", "GOOD GAME")
         sys.exit(0)
 
 
@@ -54,7 +55,7 @@ def makeBoard(n):
         cv2.line(board, (0, k), (screensize, k), (0, 0, 0), 4, cv2.LINE_AA)
 
 
-n = int(input("input number of size"))
+n = int(input("input number of size : "))
 arr = [-1] * n
 queenSize = int(screensize / (n + 1)), int(screensize / (n + 1))
 
@@ -64,10 +65,10 @@ queen = cv2.resize(queen, queenSize)
 
 makeBoard(n)
 
-cv2.namedWindow('image')
-cv2.setMouseCallback('image', on_mouse, board)
-
-cv2.imshow('image', board)
+cv2.namedWindow('board')
+cv2.setMouseCallback('board', on_mouse, board)
+cv2.imshow('board', board)
 cv2.waitKey()
+
 
 cv2.destroyAllWindows()
